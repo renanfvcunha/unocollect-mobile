988958c79df64d88ce75c76a9dbc88fdaf20baef
Renan Cunha <rfvcunha@hotmail.com>
2020-08-15 17:25:06 -0300

    Initializing Project

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettierx/default',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettierx'],
+  settings: {
+    prettierx: {
+      options: {
+        usePrettierrc: true,
+      },
+    },
+  },
+  rules: {
+    'react/style-prop-object': 'off',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+  },
+};
